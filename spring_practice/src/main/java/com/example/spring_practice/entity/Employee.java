bf3208b1055f4ad28dafeacaaef952edecd6eb31
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2022-01-14 19:43:05 +0700

    update1

---
+++ spring_practice/src/main/java/com/example/spring_practice/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.example.spring_practice.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    private int salary;
+}
